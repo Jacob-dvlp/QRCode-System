0e8033f7621e98214e8e3cd8e40611153c5e43cf
Jacob-dvlp <robsonpereirawill@gmail.com>
2023-05-02 18:33:06 +0100

    mmm

--- middlewares/admin.js
+++
@@ -1,9 +0,0 @@
-function Cliente(req, res, next){
-     if(req.session.admin != undefined){
-        next()
-    }else{
-        res.redirect("/form_login");
-    }
- }
- 
- module.exports = Cliente
--- script/script.js
+++
@@ -1,7 +0,0 @@
-const cron = require('node-cron');
-const BD = require('../config/database')
-
-cron.schedule('* * * * * *', () => {
-    console.log('Rodar em Cada minuto');
-});
-    
